84d9f061ee7e67de34c0f9dc74e0310e5fca0e52
Casey Boswell <34087449+caseyboswell@users.noreply.github.com>
2022-04-04 11:12:39 +1000

    more push tests

--- functions.js
+++ functions.js
@@ -3,6 +3,7 @@ function testFunction() {
 }
 
 function pushNotificaiton(seconds) {
+  alert('pushNotification() called!');
   var message = "Javascript push notification test"; //the message
   var button = "Open App"; //the text of the button
   window.location.href = "sendlocalpushmsg://push.send?s="+ seconds +"=msg!"+ message +"&!#"+ button +"";
